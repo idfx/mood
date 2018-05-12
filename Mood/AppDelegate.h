13b01e5c1d59d4f5447956acdbdb7b2cd8ea0d55
Juan Lara <juan@ngxinteractive.com>
2018-05-12 10:58:42 +0100

    Initial Commit

---
+++ Mood/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Mood
+//
+//  Created by Juan on 2018-05-12.
+//  Copyright © 2018 Juan Lara. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Mood/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Mood
+//
+//  Created by Juan on 2018-05-12.
+//  Copyright © 2018 Juan Lara. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
